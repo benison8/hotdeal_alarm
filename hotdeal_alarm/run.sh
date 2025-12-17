ed4bffd207c6293a5f21010a87fcdd3636a4e227
benison8 <bjc063@gmail.com>
2025-12-17 16:35:26 +0900

    a2

--- hotdeal_alarm/run.sh
+++ hotdeal_alarm/run.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+set -e
+
+# bashio 설정 값 읽기
 if [ -f /usr/bin/bashio ]; then
     INTERVAL=$(/usr/bin/bashio::config 'interval_minutes')
     URL=$(/usr/bin/bashio::config 'target_url')
@@ -6,7 +10,9 @@ else
     URL="https://www.ppomppu.co.kr/zboard/zboard.php?id=ppomppu"
 fi
 
-echo "Starting Hotdeal Monitor..."
-echo "Interval: $INTERVAL min, URL: $URL"
+echo "--- Hotdeal Monitor Starting ---"
+echo "Interval: $INTERVAL min"
+echo "URL: $URL"
 
+# 파이썬 실행 (-u 옵션으로 로그 즉시 출력)
 python3 -u /app/hotdeal_alarm.py --interval "$INTERVAL" --url "$URL"
