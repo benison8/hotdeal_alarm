101d7c5762b8abf45708dcbc9ec8696f0260c128
benison8 <63147115+benison8@users.noreply.github.com>
2025-12-17 16:41:46 +0900

    Integrate Python execution in run.sh
    Added Python execution for hotdeal_alarm script.

--- hotdeal_alarm/run.sh
+++ hotdeal_alarm/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -e
 
-# bashio 설정 값 읽기
 if [ -f /usr/bin/bashio ]; then
     INTERVAL=$(/usr/bin/bashio::config 'interval_minutes')
     URL=$(/usr/bin/bashio::config 'target_url')
@@ -14,5 +13,4 @@ echo "--- Hotdeal Monitor Starting ---"
 echo "Interval: $INTERVAL min"
 echo "URL: $URL"
 
-# 파이썬 실행 (-u 옵션으로 로그 즉시 출력)
 python3 -u /app/hotdeal_alarm.py --interval "$INTERVAL" --url "$URL"
