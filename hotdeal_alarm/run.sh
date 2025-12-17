4f82c342da6364577751db33721e556ec6e11e18
benison8 <63147115+benison8@users.noreply.github.com>
2025-12-17 15:10:45 +0900

    Update run.sh

--- hotdeal_alarm/run.sh
+++ hotdeal_alarm/run.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/with-contenv bashio
 
-# Python 종속성 설치
-pip install -r /app/requirements.txt
+# 1. Python 종속성 설치 (이미 Dockerfile에서 했다면 생략 가능하지만 안전을 위해 유지)
+pip3 install -r /app/requirements.txt
 
-# Home Assistant 설정 값 가져오기
+# 2. Home Assistant 설정 값 가져오기
+# config.yaml의 options에 정의한 이름을 넣어야 합니다.
 INTERVAL=$(bashio::config 'interval_minutes')
 URL=$(bashio::config 'target_url')
 
-bashio::log.info "Starting Hotdeal Monitor with interval: $INTERVAL minutes"
-bashio::log.info "Target URL: $URL"
+bashio::log.info "Starting Hotdeal Monitor..."
+bashio::log.info "Interval: $INTERVAL min, URL: $URL"
 
-# Python 스크립트 실행 (무한 루프)
-# Home Assistant 환경 변수를 hotdeal_monitor.py에 전달
-python3 /app/hotdeal_monitor.py --interval "$INTERVAL" --url "$URL"
+# 3. Python 스크립트 실행
+# -u 옵션을 붙여야 로그가 실시간으로 화면에 출력됩니다.
+python3 -u /app/hotdeal_alarm.py --interval "$INTERVAL" --url "$URL"
